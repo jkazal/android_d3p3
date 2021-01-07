bc807c510b35e8381fd3042956fe29061c97c050
jkazal <johann.kazal@digital-initiative.com>
2021-01-07 18:35:55 +0100

    Début vue

---
+++ app/src/main/java/com/example/rickandmorty/ui/initial/InitialViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.rickandmorty.ui.initial
+
+import android.util.Log
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.rickandmorty.data.entities.CharacterList
+import com.example.rickandmorty.data.repository.CharacterRepository
+import com.example.rickandmorty.utils.Resource
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+class InitialViewModel @ViewModelInject constructor(
+    private val repository: CharacterRepository
+) : ViewModel(), CoroutineScope {
+
+    val characters : MutableLiveData<Resource<CharacterList>> = MutableLiveData<Resource<CharacterList>>()
+
+    fun initCharacters() {
+        launch {
+            characters.postValue(repository.getCharacters())
+            Log.d("DEBUG", characters.value.toString())
+        }
+    }
+
+    override val coroutineContext: CoroutineContext
+        get() = Job() + Dispatchers.IO
+}
